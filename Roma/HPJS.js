f9c06c65e8db29c9a6388fce8788b68a1f7070be
JohnnyRock2023 <kutsenkoroma147@gmail.com>
2025-11-06 00:27:32 +0200

    HP1.0

--- Roma/HPJS.js
+++ Roma/HPJS.js
@@ -1,7 +1,9 @@
 
 var video = document.getElementById('video');
-video.volume = 0.3;
+video.volume = 0.4;
 var mute = document.getElementById('mute');
+var prevTrailerBtn = document.getElementById("prev-trailer-btn");
+var nextTrailerBtn = document.getElementById("next-trailer-btn");
 
 function changeButtonState(type) {
     if (type === "mute") {
@@ -13,3 +15,65 @@ mute.addEventListener("click", (e) => {
     video.muted = !video.muted;
     changeButtonState("mute");
 });
+
+const videos = [{ vidName: 'DMC', poster: 'DMCPoster', name: 'Диявол може плакати', desc: '2025, Фантастика', age: '16+' },
+{ vidName: 'Ballerina', poster: 'BallerinaPoster', name: 'Балерина', desc: '2025, Екшн', age: '16+' },
+{ vidName: 'Novocaine', poster: 'NovocainePoster', name: 'Новокаїн', desc: '2025, Екшн', age: '18+' },
+{ vidName: 'Dante', poster: 'DantePoster', name: 'Devil May Cry 5', desc: '2019, Слешер', age: '18+' }];
+
+var i = 0;
+
+function nextTrailer() {
+    if (i == videos.length - 1) {
+        i = 0;
+    }
+    else {
+        i++;
+    }
+    video.setAttribute('src', 'videos/' + videos[i].vidName + '.mp4');
+    video.load();
+    changePosters();
+}
+
+var leftPoster = document.getElementById('left-poster');
+var rightPoster = document.getElementById('right-poster');
+var rightTrailerName = document.getElementById('right-trailer-name');
+var rightTrailerDesc = document.getElementById('right-trailer-desc');
+
+var currTrailerName = document.getElementById('curr-trailer-name');
+var currTrailerDesc = document.getElementById('curr-trailer-desc');
+var currTrailerLimit = document.getElementById('curr-trailer-limit');
+
+function changePosters() {
+    var l = i - 1;
+    var r = i + 1;
+    if (l < 0) {
+        l = videos.length - 1;
+    }
+    if (r > videos.length - 1) {
+        r = 0;
+    }
+    leftPoster.style.backgroundImage = `url("posters/${videos[l].poster + ".jpg"}")`;
+    rightPoster.style.backgroundImage = `url("posters/${videos[r].poster + ".jpg"}")`;
+    rightTrailerName.textContent = videos[r].name;
+    rightTrailerDesc.textContent = videos[r].desc;
+    currTrailerName.textContent = videos[i].name;
+    currTrailerDesc.textContent = videos[i].desc;
+    currTrailerLimit.textContent = videos[i].age;
+}
+
+video.addEventListener("ended", nextTrailer, false);
+
+prevTrailerBtn.addEventListener("click", () => {
+    if (i == 0) {
+        i = videos.length - 1;
+    }
+    else {
+        i--;
+    }
+    video.setAttribute('src', 'videos/' + videos[i].vidName + '.mp4');
+    video.load();
+    changePosters();
+})
+
+nextTrailerBtn.addEventListener("click", nextTrailer);
